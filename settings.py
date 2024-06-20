1dca5f27eed2149f333e8d9a231688632202e176
Manan Jandial <72250914+mananjandial@users.noreply.github.com>
2024-06-20 15:04:35 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-VIDEO_PATH = os.path.join(BASE_DIR, 'video_samples', 'videotogif.mp4')
+VIDEO_PATH = os.path.join(BASE_DIR, 'video_samples', '  ')   ##PUT THE NAME OF THE INPUT VIDEO HERE
 FONT_PATH = os.path.join(BASE_DIR, 'assets', 'Roboto-Black.ttf')
 FONT_SIZE = 40
 OUTPUT_DIR = os.path.join(BASE_DIR, 'output_gifs')
